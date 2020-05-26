3ce7ea28d5881496e4d8e82de776b549ad2ce902
santosh4005 <santosh.chintamaneni@gmail.com>
2020-05-26 16:57:48 -0500

    adding cloud firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection("chats/M01LJtZwNqzYOFhSok82/messages")
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((element) {
+              print(element['text']);
+            });
+          });
+        },
+      ),
       body: ListView.builder(
           itemCount: 10,
           itemBuilder: (ctx, index) => Container(
